2a16e1998fc5c1615f6e582636ef16007a71ee9c
ll-ss <leesw0529@gmail.com>
2022-04-04 22:30:24 +0900

    add files ~session3(2)

--- session3/operator.js
+++ session3/operator.js
@@ -72,3 +72,92 @@ console.log(stringFive != numberFive);
 // strict equality, without type conversion
 console.log(stringFive === numberFive);
 console.log(stringFive !== numberFive);
+
+// object equality by reference
+const mark1 = { name : 'mark' };
+const mark2 = { name : 'mark' };
+const mark3 = mark1;
+console.log(mark1 == mark2);
+console.log(mark1 === mark2);
+console.log(mark1 === mark3);
+
+// equality - puzzler
+console.log(0 == false);
+console.log(0 === false);
+console.log('' == false);
+console.log('' === false);
+console.log(null == undefined);
+console.log(null === undefined);
+
+// 8. Conditional operators: if
+// if, else if, else
+
+const name = 'mark';
+if (name === 'mark') {
+  console.log('Welcome, Mark!');
+} else if (name === 'coder') {
+  console.log('You are amazing coder');
+} else {
+  console.log('unknown');
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+
+console.log(name === 'mark' ? 'yes' : 'no');
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+
+const browser = 'IE';
+switch (browser) {
+  case 'IE':
+    console.log('go away!');
+    break;
+  case 'Chrome':
+    console.log('love you!');
+    break;
+  case 'Firefox':
+    console.log('love you!');
+    break;
+  default:
+    console.log('same all!');
+    break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is excuted
+
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+
+// do while loop, body code is excuted first,
+// then check the condition
+
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+  console.log(`for: ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+  // inline variable declaration
+  console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`i: ${i}, j: ${j}`);
+  }
+}
